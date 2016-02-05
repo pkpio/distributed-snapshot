a4b610cd4b69d7db844400addccc8246bc14fad7
rkamath3 <aramachandrakamath@gmail.com>
2016-02-05 13:35:40 +0100

    common SnapShot file

---
+++ src/com/tk/ds/common/Snapshot.java
@@ -0,0 +1,17 @@
+package com.tk.ds.common;
+
+import com.tk.ds.ui.DsUi;
+
+public class Snapshot {
+
+	DsUi snapshotUi;
+
+	public Snapshot(DsUi snapshotUi) {
+		this.snapshotUi = snapshotUi;
+	}
+
+	void updateInfoPanel(String info) {
+
+	}
+
+}
